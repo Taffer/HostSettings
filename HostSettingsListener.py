f98e608b8482bff5bbf9e220520fb4a54469a99c
Chris Herborth <chrish@taffer.ca>
2026-03-01 07:01:21 -0500

    Add type hints

--- HostSettingsListener.py
+++ HostSettingsListener.py
@@ -4,9 +4,13 @@
 # Posted by OdatNurd
 # Retrieved 2026-02-20, License - CC BY-SA 4.0
 
+# TODO: Python 3.7+, not necessary in 3.14+
+from __future__ import annotations
+
 import socket
 import sublime
 import sublime_plugin
+import typing
 
 # TODO: Is there any way to call Package Control to disable/enable packages?
 #
@@ -17,16 +21,16 @@ import sublime_plugin
 class HostSettingsListener(sublime_plugin.EventListener):
     """Tweak settings per-host when necessary."""
 
-    preferences = None
-    hostname = None
+    preferences: sublime.Settings | None = None
+    hostname: str | None = None
 
-    def settings_changed(self):
+    def settings_changed(self: HostSettingsListener) -> None:
         """Update every view in every window if the settings change."""
         for window in sublime.windows():
             for view in window.views():
                 self.on_new(view)
 
-    def on_init(self, views):
+    def on_init(self: HostSettingsListener, views: typing.List[sublime.View]) -> None:
         """Update settings when the plugin is initialized."""
         if not self.hostname:
             self.hostname = socket.gethostname() or "localhost"
@@ -42,7 +46,7 @@ class HostSettingsListener(sublime_plugin.EventListener):
         for view in views:
             self.on_new(view)
 
-    def on_new(self, view):
+    def on_new(self: HostSettingsListener, view: sublime.View) -> None:
         """Update settings when a new view is created."""
         host_prefs = self.preferences.get("host-specific", {})
         prefs = host_prefs.get(self.hostname, {})
